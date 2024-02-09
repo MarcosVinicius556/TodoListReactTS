092dcaeff865cc2525e4f77bc6ba73736f679be2
marcos <marcosvinicios4132@gmail.com>
2024-02-09 15:40:06 -0300

    Corpo do modal criado

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal';
 
 //Interfaces
 import { ITask } from './interfaces/Task';
@@ -27,6 +28,7 @@ function App() {
 
   return (
       <div>
+        <Modal /> 
         <Header />
         <main className={styles.main}>
           <div>
---
+++ src/components/Modal.tsx
@@ -0,0 +1,18 @@
+import styles from './Modal.module.css';
+
+interface Props {
+
+}
+
+const Modal = (props: Props) => {
+  return (
+    <div id="modal">
+        <div className=""></div>
+        <div>
+            <h2>texto modal</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
