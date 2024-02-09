a8dd608f55b5677da0c290cfa2e642e93f5ff0e7
marcos <marcosvinicios4132@gmail.com>
2024-02-09 15:09:55 -0300

    Exibição inicial das listas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,27 @@
+//Interfaces
+import { ITask } from "../interfaces/Task"
 
-interface Props {}
+//CSS
+import styles from './TaskList.module.css';
 
-const TaskList = (props: Props) => {
+interface Props {
+  taskList: ITask[]
+}
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>TaskList</div>
+    <>
+      {
+        taskList.length > 0 ? (
+          taskList.map((task) => (
+            <div key={task.id}>
+              {task.title}
+            </div>
+          ))
+        ) : (
+          <p>Nenhuma tarefa cadastrada</p>
+        )}
+    </>
   )
 }
 
